d0df4c4a1b5593795275f29501e51f3585dd4e3a
Nick <nickjanquart@gmail.com>
2016-12-27 16:45:55 -0700

    authentication

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, redirect, url_for
+from flask import render_template, request, redirect, url_for, flash
+from flask_login import login_user
+from werkzeug.security import check_password_hash
 
 from . import app
-from .database import session, Entry
+from .database import session, Entry, User
 
-PAGINATE_BY = 10
+PAGINATE_BY = 5
 
 @app.route("/")
 @app.route("/page/<int:page>")
@@ -87,6 +89,24 @@ def delete_entry_post(id):
 
     session.commit()
     return redirect(url_for("entries"))
+
+@app.route("/login", methods=["GET"])
+def login_get():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login_post():
+    email = request.form["email"]
+    password = request.form["password"]
+    user = session.query(User).filter_by(email=email).first()
+    if not user or not check_password_hash(user.password, password):
+        flash("Incorrect username or password", "danger")
+        return redirect(url_for("login_get"))
+
+    login_user(user)
+    return redirect(request.args.get('next') or url_for("entries"))
+
+
 
 
 
