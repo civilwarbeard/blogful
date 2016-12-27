277c442a798d16950bcb5495a0d8de213dcc03e5
Nick <nickjanquart@gmail.com>
2016-12-27 16:42:45 -0700

    pushing authentication test

--- blog/database.py
+++ blog/database.py
@@ -3,8 +3,9 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
 
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from . import app
+from flask_login import UserMixin
 
 engine = create_engine(app.config["SQLALCHEMY_DATABASE_URI"])
 Base = declarative_base()
@@ -18,5 +19,14 @@ class Entry(Base):
     title = Column(String(1024))
     content = Column(Text)
     datetime = Column(DateTime, default=datetime.datetime.now)
+
+class User(Base):
+	__tablename__= "users"
+
+	id = Column(Integer, primary_key=True)
+	name = Column(String(128))
+	email = Column(String(128), unique=True)
+	password = Column(String(128))
+	is_active = Column(Boolean, default=True)
 
 Base.metadata.create_all(engine)
