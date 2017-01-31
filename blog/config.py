18b2ee6c0df0a4dc4676be4c3b584c79ac1dc770
Nick <nickjanquart@gmail.com>
2017-01-30 19:53:58 -0600

    trying out blogzo for heroku

--- blog/config.py
+++ blog/config.py
@@ -1,8 +1,8 @@
 import os
 
 class DevelopmentConfig(object):
-	SQLALCHEMY_DATABASE_URI = "postgresql://ubuntu:Cthulhu81@localhost:5432/blogful"
-	DEBUG = True
+	SQLALCHEMY_DATABASE_URI = "    postgres://qgtmtzhohhhnoh:f1c07c2b6b6fe956086d61572dc5659d3ed1ff6cdccd6913a771edf5a436b991@ec2-23-23-225-116.compute-1.amazonaws.com:5432/d92okdk8muv3nh"
+	DEBUG = False
 	SECRET_KEY = os.environ.get("BLOGFUL_SECRET_KEY", os.urandom(12))
 
 class TestingConfig(object):
